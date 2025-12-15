2e87d100337b1c75b3ef9d42ca4e161cd1cca588
Naveen G <gangineninaveen132@gmail.com>
2025-12-16 05:10:34 +0530

    securitygroup_egress_rule

--- main.tf
+++ main.tf
@@ -4,6 +4,15 @@ resource "aws_security_group" "main" {
     description = var.sg_discription   #optional, but we are making as mandatory for practice purpose
     vpc_id      = var.vpc_id
 
+# aws security group, if we are not giving outboubd traffic, we cant connect to the internet, so its mantaory  to give outbound rule..
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
     tags = merge(
         var.sg_tags,
         local.common_tags,
@@ -13,3 +22,4 @@ resource "aws_security_group" "main" {
 
     )
 }
+
